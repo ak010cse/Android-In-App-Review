5ee3b78eb9a4fed8d889b3b9919775bdcee832ca
ak010cse <ak010cse@gmail.com>
2023-08-22 10:18:02 +0530

    Initial commit

---
+++ app/src/main/java/com/android/in_appreview/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.android.in_appreview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.play.core.review.ReviewException
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.review.model.ReviewErrorCode
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val manager = ReviewManagerFactory.create(this)
+
+        val request = manager.requestReviewFlow()
+        request.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // We got the ReviewInfo object
+                val reviewInfo = task.result
+                val flow = manager.launchReviewFlow(this, reviewInfo)
+                flow.addOnCompleteListener { _ ->
+                    // The flow has finished. The API does not indicate whether the user
+                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                    // matter the result, we continue our app flow.
+                }
+            } else {
+                // There was some problem, continue regardless of the result.
+                // you can show your own rate dialog alert and redirect user to your app page
+                // on play store.
+            }
+        }
+    }
+}
